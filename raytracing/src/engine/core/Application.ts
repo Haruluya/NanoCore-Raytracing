a3a45b1fb338a0bee5545c1eebd0ed505be70f8b
Haruluya <2198645565@qq.com>
2022-11-09 20:19:10 +0800

    init project and add math module.

---
+++ raytracing/src/engine/core/Application.ts
@@ -0,0 +1,47 @@
+
+namespace NanoRayTracing{
+    interface AppSpecification{
+        name:string,
+        version:string,
+    }
+
+
+    export class Application{
+        private m_Specification:AppSpecification;
+		private m_Running:boolean = false;
+		private m_LastFrameTime:number = 0.0;
+
+
+
+        constructor(){
+
+        }
+        PushLayer():void{
+
+        }
+        PushOverlay():void{
+
+        }
+        static GetInstance():Application{
+
+        }
+        GetSpecification():AppSpecification{
+
+        }
+        Run():void{
+            this.m_Running = true;
+            
+
+            //Update.
+            // for (auto& layer : m_LayerStack)
+            //     layer.OnUpdate(m_TimeStep);
+
+            //Timestep.
+            // float time = GetTime();
+			// m_FrameTime = time - m_LastFrameTime;
+			// m_TimeStep = glm::min<float>(m_FrameTime, 0.0333f);
+			// m_LastFrameTime = time;
+        }
+
+    }
+}
---
+++ raytracing/src/pages/RayTracing.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="RayTracing">
+        <canvas class="show" ref="canvas" width="800" height="800"/>
+        <canvas class="textureCanvas" ref="texture" width="1024" height="1024">
+        </canvas>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent, reactive, ref,onMounted} from 'vue';
+
+/// <reference path="../engine/core/Application.ts" />
+import Application = NanoRayTracing.Application
+
+
+export default defineComponent({
+  name: 'RayTracing',
+  setup(){
+    const engine:Application = ref<Application>(new Application());
+
+
+    onMounted(()=>{
+        engine.Run();
+
+    })
+  }
+})
+</script>
+<style lang="less" scoped>
+
+</style>
